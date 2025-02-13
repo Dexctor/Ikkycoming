5d74e8d7d701624d5b1da4684ae1c35ff2e4e5b5
dexctor <contactantoinedewas@gmail.com>
2025-02-14 00:05:41 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,15 +42,8 @@ export const metadata: Metadata = {
     images: [`${BASE_URL}/images/og-image.jpg`],
   },
   robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
+    index: false,
+    follow: false,
   },
   other: {
     'bfcache-opt-in': 'true'
