517965942d28504baeca8b6269deaf4705a1ca70
dexctor <contactantoinedewas@gmail.com>
2025-02-14 13:22:21 +0100

    fix

--- src/app/constants/features.tsx
+++ src/app/constants/features.tsx
@@ -18,7 +18,7 @@ export const FEATURES: Feature[] = [
     title: "Gérer vos Jumeaux Numériques",
     desc: (
       <>
-        Exploitez des informations détaillées et conformes aux normes pour une visualisation et une conception précises, et valorisez vos projets immobiliers grâce à la {' '}
+        Exploitez des informations détaillées et conformes aux normes pour une visualisation, une conception précises, et valorisez vos projets immobiliers grâce à la {' '}
         <span className="text-green-500 font-medium">Tokenisation</span>.
       </>
     ),
