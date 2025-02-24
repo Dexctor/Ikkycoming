bb0c0f34037583312b2e4121978a9971fb952ab4
dexctor <contactantoinedewas@gmail.com>
2025-02-24 20:08:53 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://myikki.com';
 // Google Analytics ID
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
+// Pixels publicitaires
+const FACEBOOK_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+const LINKEDIN_PIXEL_ID = process.env.NEXT_PUBLIC_LINKEDIN_PIXEL_ID;
+const TWITTER_PIXEL_ID = process.env.NEXT_PUBLIC_TWITTER_PIXEL_ID;
+
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
   title: "MyIKKI - L'Immobilier Réinventé par les Jumeaux Numériques",
@@ -96,14 +101,72 @@ export default function RootLayout({
           </>
         )}
 
+        {/* Twitter/X Pixel */}
+        {TWITTER_PIXEL_ID && (
+          <Script id="twitter-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){
+                e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},
+                s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))
+              }(window,document,'script');
+              twq('config', '${TWITTER_PIXEL_ID}');
+            `,
+          }} />
+        )}
+
+        {/* Facebook Pixel */}
+        {FACEBOOK_PIXEL_ID && (
+          <>
+            <Script id="facebook-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s){
+                  if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments);};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(n)[0];
+                  s.parentNode.insertBefore(t,s)
+                }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${FACEBOOK_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `,
+            }} />
+            <noscript>
+              <img height="1" width="1" style={{ display: "none" }} src={`https://www.facebook.com/tr?id=${FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`} />
+            </noscript>
+          </>
+        )}
+
+        {/* LinkedIn Pixel */}
+        {LINKEDIN_PIXEL_ID && (
+          <>
+            <Script id="linkedin-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+              __html: `
+                _linkedin_partner_id = "${LINKEDIN_PIXEL_ID}";
+                window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+              `,
+            }} />
+            <Script id="linkedin-tracking" strategy="afterInteractive" dangerouslySetInnerHTML={{
+              __html: `
+                (function(l) {
+                  if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}
+                  var s = document.getElementsByTagName("script")[0];
+                  var b = document.createElement("script");
+                  b.type = "text/javascript";b.async = true;
+                  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                  s.parentNode.insertBefore(b, s);
+                })(window.lintrk);
+              `,
+            }} />
+            <noscript>
+              <img height="1" width="1" style={{ display: "none" }} alt="" src={`https://px.ads.linkedin.com/collect/?pid=${LINKEDIN_PIXEL_ID}&fmt=gif`} />
+            </noscript>
+          </>
+        )}
+
         {/* Préchargement optimisé de l'image du logo */}
-        <link
-          rel="preload"
-          href="/images/logo/logo.webp"
-          as="image"
-          type="image/webp"
-          fetchPriority="high"
-        />
+        <link rel="preload" href="/images/logo/logo.webp" as="image" type="image/webp" fetchPriority="high" />
       </head>
       <body className={inter.className}>
         {children}
